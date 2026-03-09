f0cd75ae0711f25b225ffc2e9e18e7f223c0c845
Brogis1 <sokolov.igor.ch@gmail.com>
2026-03-09 11:24:34 +0100

    Bump version

--- pltx/__init__.py
+++ pltx/__init__.py
@@ -1,6 +1,6 @@
 """pltx - Enhanced Matplotlib for Scientific Visualization."""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # Auto-register pasqal colormaps on import
 from .cmap import register_pasqal_cmap as _register_pasqal_cmap
